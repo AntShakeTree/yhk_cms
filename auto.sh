788e22d859e6cb9db77f714c420d3cce22a5f9ea
ant_shake_tree <ant_shake_tree@163.com>
2015-09-14 15:21:31 +0800

    Api 与自动部署脚本提交

--- auto.sh
+++ auto.sh
@@ -3,10 +3,12 @@
 killall java
 echo $(pwd)
 mvn clean
+mvn compile
 mvn package -Dmaven.test.skip=true
-rm -rf /Users/ant_shake_tree/Documents/apache-tomcat-8.0.23/webapps/yhk_cms*
-mv $(pwd)/target/ykh_cms*.war /Users/ant_shake_tree/Documents/apache-tomcat-8.0.23/webapps/yhk_cms.war
+#rm -rf /Users/ant_shake_tree/Documents/apache-tomcat-8.0.23/webapps/yhk_cms*
+mv $(pwd)/target/ykh_cms*.war $(pwd)/target/ykh_cms.war
+sshpass -p SOONcore1234  scp  -r $(pwd)/target/ykh_cms.war root@123.56.125.184:/data/
 
-/Users/ant_shake_tree/Documents/apache-tomcat-8.0.23/bin/startup.sh
+#/Users/ant_shake_tree/Documents/apache-tomcat-8.0.23/bin/startup.sh
 
 mvn compile
